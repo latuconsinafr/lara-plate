e70c31264f2fafa7055b73182206234330e45cc5
latuconsinafr <faristalatuconsina@gmail.com>
2020-02-04 19:45:14 +0700

    modify basic laravel config

--- config/auth.php
+++ config/auth.php
@@ -42,9 +42,8 @@ return [
         ],
 
         'api' => [
-            'driver' => 'token',
+            'driver' => 'passport',
             'provider' => 'users',
-            'hash' => false,
         ],
     ],
 
@@ -68,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\User::class,
+            'model' => App\Models\User::class,
         ],
 
         // 'users' => [
